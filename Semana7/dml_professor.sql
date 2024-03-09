c7b9b93f3aa85b157961ecca00c90a81e4c86b4c
Bruna Leal <brumainardes.bl@gmail.com>
2024-03-08 23:36:42 -0300

    exercício 3

---
+++ Semana7/dml_professor.sql
@@ -0,0 +1,16 @@
+INSERT INTO professor (id_professor, nome, especialidade)
+VALUES (1, 'Layne Staley','Química');
+
+INSERT INTO professor (id_professor, nome, especialidade)
+VALUES (2, 'Bruce Dickinson','Física');
+
+INSERT INTO professor (id_professor, nome, especialidade)
+VALUES (3, 'Cristina Scabbia','Matemática');
+
+INSERT INTO professor (id_professor, nome, especialidade)
+VALUES (4, 'Eddie Vedder','Filosofia');
+
+INSERT INTO professor (id_professor, nome, especialidade)
+VALUES (5, 'Floor Jansen','Artes');
+
+SELECT * FROM professor;
