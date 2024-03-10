153c0631435a66825e8cd881470130a3f4c68013
Bruna Leal <brumainardes.bl@gmail.com>
2024-03-10 15:34:10 -0300

    exercício 4 - ddl curso

---
+++ Semana7/ddl_curso.sql
@@ -0,0 +1,8 @@
+CREATE TABLE curso (
+	id_curso INTEGER NOT NULL,
+	nome VARCHAR (100),
+	descricao VARCHAR (100),
+	professores VARCHAR (100),
+	carga_horaria VARCHAR (100),
+	PRIMARY KEY (id_curso)
+)
