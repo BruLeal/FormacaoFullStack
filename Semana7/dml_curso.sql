f29c4cdf3d60fc41ef269be74838471293179d4b
Bruna Leal <brumainardes.bl@gmail.com>
2024-03-10 18:31:32 -0300

    exercicio 5

---
+++ Semana7/dml_curso.sql
@@ -0,0 +1,13 @@
+INSERT INTO curso (id_curso, nome, descricao, carga_horaria)
+VALUES (1, 'quimica', 'Curso de Química para Ensino Médio', '5');
+
+INSERT INTO curso (id_curso, nome, descricao, carga_horaria)	
+VALUES (2, 'fisica', 'Curso de Física para Ensino Médio', '7' );
+
+INSERT INTO curso (id_curso, nome, descricao, carga_horaria)
+VALUES (3, 'matemática', 'Curso de Matemática para Ensino Médio', '6');
+
+SELECT * FROM curso;
+	
+ 
+ 
